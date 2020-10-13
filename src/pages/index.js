c79432360e94ecc8104da5f574403f55da3e4262
Carlos H <carlosh@mailbox.org>
2020-10-13 15:46:01 -0300

    Remove unneeded JS

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,12 @@ import React from "react"
 import Page from "../components/landing-pages/rocket/landing"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import VendorsHeader from "../components/landing-pages/rocket/vendors"
+//import VendorsHeader from "../components/landing-pages/rocket/vendors"
 import Header from "../components/header"
 
 const IndexPage = () => (
     <Layout>
         <SEO title="Home" />
-        <VendorsHeader/>
         <Header/>
         <Page/>
     </Layout>
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,14 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import LoginPage from "../components/landing-pages/rocket/login"
-import VendorsHeader from "../components/landing-pages/rocket/vendors"
 import Header from "../components/header"
 import SEO from "../components/seo"
 
 const Page = () => (
     <Layout>
         <SEO title="Log in" />
-        <VendorsHeader/>
         <Header/>
         <LoginPage/>
     </Layout>
