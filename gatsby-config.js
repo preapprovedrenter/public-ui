d676d5a86f2224e5cd70aa83001d7fbe8b039541
Carlos H <carlosh@mailbox.org>
2020-10-15 10:54:09 -0300

    Give a hint on datatype of apiUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    apiUrl: process.env.PREAPP_API_URL,
+    apiUrl: `${process.env.PREAPP_API_URL}`,
   },
   plugins: [
     `gatsby-plugin-sass`,
