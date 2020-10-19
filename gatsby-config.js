75727935bc2ad019cde1b719116e3d7a78ae2816
Carlos H <carlosh@mailbox.org>
2020-10-19 16:22:32 -0300

    Adjusting site configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,15 @@
 const activeEnv =
   process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
 
-console.log(`Using environment config: '${activeEnv}'`)
-
 require("dotenv").config({
   path: `.env.${activeEnv}`,
 })
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `PreApproved Renter`,
+    description: `Screen Tenants Instantly.`,
+    author: `@preapprovedrenter`,
     apiUrl: `${process.env.PREAPP_API_URL}`,
     cookieDomain: `${process.env.PREAPP_COOKIE_DOMAIN}`,
   },
@@ -38,13 +36,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `PreApprovedRenter`,
+        short_name: `PreApp`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/img/brand/icon.png`, // This path is relative to the root of the site.
       },
     },
     {
